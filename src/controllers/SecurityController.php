7d72ce4dffc14c50a3ba32ae41264b582a422c77
Lucas Martignon <lucas.martignon@wanadoo.fr>
2023-07-05 14:53:43 +0200

    create login and subscription form

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -3,12 +3,51 @@
 namespace App\Controllers;
 
 use Ostyna\Component\Framework\AbstractPageController;
+use Ostyna\Component\Framework\Form\FormArchitect;
+use Ostyna\Component\Framework\Form\InputEmail;
+use Ostyna\Component\Framework\Form\InputPassword;
+use Ostyna\Component\Framework\Form\InputSubmit;
+use Ostyna\Component\Framework\Form\InputText;
+use Ostyna\Component\Framework\Form\Label;
 
 class SecurityController extends AbstractPageController 
 {
   public function display() {
-    return $this->render('/web/mainpage.html', [
-      'controller' => "Controller SecurityController",
+
+    $login_form = new FormArchitect();
+    $login_form
+      ->add(new InputText('username', new Label(content: "Nom d'utilisateur"), "", [
+        'minlength' => [
+          'value' => 5,
+          'message' => "Doit contenir au moins %v caractères"
+        ]
+      ]))
+      ->add(new InputPassword('password', new Label(content: "Mot de Passe"), "", [
+      ]))
+      ->add(new InputSubmit(value: "Envoyer"));
+
+    return $this->render('/web/index_security.html', [
+      'loginForm' => $login_form->build(),
+    ]);
+  }
+
+  public function subscribe() {
+    $sub_form = new FormArchitect();
+    $sub_form
+      ->add(new InputText('username', new Label(content: "Nom d'utilisateur"), "", [
+        'minlength' => [
+          'value' => 5,
+          'message' => "Doit contenir au moins %v caractères"
+        ]
+      ]))
+      ->add(new InputEmail('email', new Label(content: "Email"), "", [
+      ]))
+      ->add(new InputPassword('password', new Label(content: "Mot de Passe"), "", [
+      ]))
+      ->add(new InputSubmit(value: "Envoyer"));
+
+    return $this->render('/web/index_subscription.html', [
+      'subForm' => $sub_form->build(),
     ]);
   }
 }
